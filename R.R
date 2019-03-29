ad777cb06e4b6a6822415d06428d7df35bcc5ba1
Darfool <siusialfool@gmail.com>
2019-03-29 12:11:58 +0100

    commit

--- R.R
+++ R.R
@@ -1,2 +1,17 @@
+library(DBI)
+library(RMySQL)
+library(dplyr)
+library(dbplyr)
+con <- DBI::dbConnect(RMySQL::MySQL(),host = "remotemysql.com",user = "ItTFpYEKBn",password = "VxkzjjE3Bd",dbname="ItTFpYEKBn")
 
-setwd("D:/R")
+#dans flights_db on cole la table flights2?
+flights_db <- tbl(con, "flights2")
+
+# un select
+flights_db %>% select(tailnum, origin, dest)
+
+# select * from flights2 where hour < 10
+flights_db %>% filter(hour < 10)
+
+# montre la requette sql
+flights_db %>% show_query()
